f0ee8628a3c1fdc37456588c21b21d640f96d432
Isfirs <isfirs.pb@gmail.com>
2016-08-25 14:09:54 +0200

    Reached 180 characters

--- src/org/company/MySuperLongClass.java
+++ src/org/company/MySuperLongClass.java
@@ -11,7 +11,7 @@ public class MySuperLongClass {
         //
     }
 
-    public MySuperLongClass(int one, String two, Object three, long four, double five, float six, MyEnum seven, MyClass eight, AbstractMyClass nine) {
+    public MySuperLongClass(final int one, final String two, final Object three, final long four, final double five, final float six, final MyEnum seven, final MyClass eight, final AbstractMyClass nine) {
         //
     }
 
